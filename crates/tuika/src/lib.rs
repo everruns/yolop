dc7fc329a12a4c1412e6558db4f7bd27683d81fe
Mykhailo Chalyi <mike@chaliy.name>
2026-07-24 04:37:57 +0000

    feat(tuika): QrCode component with byte-mode v1-4 encoder

--- crates/tuika/src/lib.rs
+++ crates/tuika/src/lib.rs
@@ -84,11 +84,11 @@ pub use components::{
     ASCII_FONT_HEIGHT, AsciiFont, Boxed, CONSOLE_DEFAULT_CAPACITY, CodeBlock, Column, Console,
     ConsoleLog, Constrained, Diff, DiffMode, DiffRow, DiffStyle, DiffTag, Flex, FocusScope,
     ImageResolver, KeyHints, Loader, Markdown, MarkdownImage, MarkdownState, Paragraph,
-    ProgressBar, Responsive, Rule, Scroll, ScrollState, SelectList, SelectOutcome, SelectState,
-    Slider, SliderState, Spacer, Spinner, SpinnerStyle, StatusBar, TOAST_DEFAULT_TTL, TabSelect,
-    TabSelectOutcome, TabSelectState, Table, Tabs, TabsState, Text, TextInput, TextInputEvent,
-    TextInputMode, TextInputState, ToastLevel, ToastList, Toasts, Wrap, diff_rows,
-    markdown_to_lines, wrap_lines,
+    ProgressBar, QrCode, QrEcc, Responsive, Rule, Scroll, ScrollState, SelectList, SelectOutcome,
+    SelectState, Slider, SliderState, Spacer, Spinner, SpinnerStyle, StatusBar, TOAST_DEFAULT_TTL,
+    TabSelect, TabSelectOutcome, TabSelectState, Table, Tabs, TabsState, Text, TextInput,
+    TextInputEvent, TextInputMode, TextInputState, ToastLevel, ToastList, Toasts, Wrap, diff_rows,
+    markdown_to_lines, qr_encode, wrap_lines,
 };
 pub use event::{Event, EventFlow, Key, KeyCode, Mouse, MouseButton, MouseKind};
 pub use focus::FocusRegistry;
